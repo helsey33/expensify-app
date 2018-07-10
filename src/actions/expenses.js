333c194f90b9d4a898943f7f154036d5265732cc
helsey33 <helseysin827@gmail.com>
2018-07-10 11:08:40 +0530

    edit and remove through firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -24,16 +24,35 @@ return (dispatch)=>{
 }
 }
 
+//REMOVE_EXPENSES
 export const removeExpense = (id) => ({
     type: 'REMOVE_EXPENSE',
     id
 });
 
+export const startRemoveExpense = (id) =>{
+    return (dispatch)=>{
+        database.ref('expenses/'+id).remove().then(()=>{
+            dispatch(removeExpense(id));
+        })
+    }
+}
+
+
+//EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+export const startEditExpense = (id,updates) =>{
+    return (dispatch)=>{
+        database.ref('expenses/'+id).update(updates).then(()=>{
+            dispatch(editExpense(id,updates));
+        })
+    }
+}
 
 //SET EXPENSES
 const setExpenses = (expenses)=>({
--- src/components/editExpense.js
+++ src/components/editExpense.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense } from './../actions/expenses';
-import { removeExpense } from './../actions/expenses';
+import { startEditExpense } from './../actions/expenses';
+import { startRemoveExpense } from './../actions/expenses';
+
+
 const EditExpensePage = (props) => {
      
      return(
@@ -10,12 +12,12 @@ const EditExpensePage = (props) => {
             <ExpenseForm 
             expense={props.expense}
              onSubmit={(expense)=>{
-                 props.dispatch(editExpense(props.expense.id,expense));
+                 props.dispatch(startEditExpense(props.expense.id,expense));
                  props.history.push('/');
              }}
              />
              <button onClick={() => {
-                 props.dispatch(removeExpense(props.expense.id));
+                 props.dispatch(startRemoveExpense(props.expense.id));
                  props.history.push('/');
              }}> Remove </button>
          </div>
